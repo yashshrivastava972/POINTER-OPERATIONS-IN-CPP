dc58b2af49e04eef9232e51f097f2f7971ee608d
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-24 13:18:02 +0530

    Update PROGRAM 10A.cpp

--- PROGRAM 10A.cpp
+++ PROGRAM 10A.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+void swapValues(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+int main() {
+    int a = 27, b = 67;
+    swapValues(a, b); 
+    cout << a << " " << b << endl; 
+    return 0;
+}
+
+// OUTPUT:
+// 27 67
 
