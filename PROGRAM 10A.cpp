64baf9be0efed5e5b107ff00b9bf484d73f12df7
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-24 13:28:12 +0530

    Update PROGRAM 10A.cpp

--- PROGRAM 10A.cpp
+++ PROGRAM 10A.cpp
@@ -1,3 +1,10 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: POINTER OPERATION - CALL BY VALUE IN C++ (EXPERIMENT: 10A)
+
+// CODE: 
+
 #include <iostream>
 using namespace std;
 void swapValues(int x, int y) {
